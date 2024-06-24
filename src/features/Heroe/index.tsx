e435444b06f792d0558a7a4782865de9ed2a67cd
abluedev <alexbluedev@gmail.com>
2024-06-24 22:38:53 +0200

    wip hero panel

--- src/features/Heroe/index.tsx
+++ src/features/Heroe/index.tsx
@@ -14,7 +14,25 @@ interface ElementProps {
 export const Heroe = ({ config, action }: HeroeProps & ElementProps) => {
 	return (
 		<section className={styles["scenario_zone--heroe"]}>
+
+			<div className={styles["heroe--img"]}>
 				<img src={config.avatar} alt={"Aniv heroe"}/>
+				<img src={"/assets/heroe/brush.svg"} alt={""} className={styles["effect"]}/>
+			</div>
+			<section className={styles["heroe--panel"]}>
+				<section className={styles["heroe--stats"]}>
+					<button
+						className={`${styles["heroe_stat"]} ${styles["heroe_action--hp"]}`}
+					>
+						HP
+					</button>
+					<button
+						className={`${styles["heroe_stat"]} ${styles["heroe_action--ap"]}`}
+					>
+						AP
+
+					</button>
+				</section>
 				<section className={styles["heroe--actions"]}>
 					<button
 						className={`${styles["heroe_action"]} ${styles["heroe_action--attack"]}`}
@@ -34,7 +52,29 @@ export const Heroe = ({ config, action }: HeroeProps & ElementProps) => {
 						<GiBoltShield className={`${styles["heroe_action--icon"]}`}/>
 
 					</button>
+					<button
+						className={`${styles["heroe_action"]} ${styles["heroe_action--defense"]}`}
+						onClick={() => {
+
+							(action.current = "DEFENSE")
+						}}
+					>
+						Defense
+						<GiBoltShield className={`${styles["heroe_action--icon"]}`}/>
+
+					</button><button
+						className={`${styles["heroe_action"]} ${styles["heroe_action--defense"]}`}
+						onClick={() => {
+
+							(action.current = "DEFENSE")
+						}}
+					>
+						Defense
+						<GiBoltShield className={`${styles["heroe_action--icon"]}`}/>
+
+					</button>
 				</section>
+			</section>
 		</section>
 	);
 };
